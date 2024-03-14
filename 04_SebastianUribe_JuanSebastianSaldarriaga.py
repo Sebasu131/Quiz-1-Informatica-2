4498bb499a387fc9b745cb1694e76572cda0286a
Sebasu131 <sebastian.uribez@udea.edu.co>
2024-03-13 21:37:38 -0500

    Clases Heredadas ProtesisCadera y Marcapasos

--- 04_SebastianUribe_JuanSebastianSaldarriaga.py
+++ 04_SebastianUribe_JuanSebastianSaldarriaga.py
@@ -20,3 +20,20 @@ class ImplanteMedico:
         self.__tipo_fijacion=fi
     def asignartamano(self,M):
         self.__tamano=M
+
+class ProtesisCadera(ImplanteMedico):
+    def __init__(self, material, tipo_fijacion, tamano):
+        super().__init__("Prótesis de cadera", material, tipo_fijacion, tamano)
+        
+class Marcapasos(ImplanteMedico):
+    def __init__(self, material, tipo_fijacion, tamano, num_electrodos, alambrico, frecuencia):
+        super().__init__("Marcapasos", material, tipo_fijacion, tamano)
+        self.__num_electrodos = num_electrodos
+        self.__alambrico = alambrico
+        self.__frecuencia = frecuencia
+    def verelectrodos(self):
+        return self.__num_electrodos
+    def veralambrico(self):
+        return self.__alambrico
+    def verfrecuencia(self):
+        return self.__frecuencia
